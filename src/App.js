71af13fb8330084555ad106506ef258b5fae0dbb
Vladis Markin <vladismarkin@gmail.com>
2021-10-03 17:37:33 +0300

    Docker Compose for Running Tests

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi :)</p>
+        <p>I was changed!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
